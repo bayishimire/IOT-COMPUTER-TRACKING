ca439fee083d6946dd785733cc19babf84798be5
bayishimire <bayishimiresamuel760@gmail.com>
2025-05-14 23:19:16 +0200

    fghsgfgh

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,134 @@
-#include <Arduino.h>
+#define CAMERA_MODEL_AI_THINKER 
+#include "esp_camera.h"
 #include <WiFi.h>
 
-const char* ssid="SOGO7";
-const char* password="eze12345";
+// 📶 Set your WiFi credentials
+const char* ssid = "Starlink-2G";
+const char* password = "Kigali#0987";
 
-WiFiServer server(80);
+// 📸 Define camera pins for AI Thinker model
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+void startCameraServer();
 
 void setup() {
-Serial.begin(115200);
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
 
-Serial.println("connect to wifi");
-Serial.println(ssid);
-WiFi.begin(ssid,password);
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println(WiFi.localIP());
 
-while (WiFi.status() !=WL_CONNECTED){
-delay(500);
-Serial.print(".");
+  // Camera configuration
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  // Select frame size & quality
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_QVGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_QVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // Initialize camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  // Start streaming server
+  startCameraServer();
+  Serial.println("Camera ready! Stream at:");
+  Serial.print("http://");
+  Serial.println(WiFi.localIP());
 }
-Serial.println("");
-Serial.println("wifi connectd");
-Serial.println("esp32 ip address:");
-Serial.println(WiFi.localIP());
 
-//dealing with server connection
-server.begin();
+#include <WebServer.h>
+WebServer server(80);
 
-}
+void handleStream() {
+  WiFiClient client = server.client();
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  String response = "HTTP/1.1 200 OK\r\n";
+  response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
+  server.sendContent(response);
 
-
-WiFiClient client = server.available();
-
-if(client){
-
-  String currentline = "";
-  while(client.connected()){
-    if(client.available()){
-      char d;
-      d = client.read();
-      Serial.write(d);
-
-
-    if(d=='\n'&&currentline.length()==0){
-      client.println("HTTP/1.1 200 OK");
-      client.println("Content-type: text/html");
-      // client.println("Connection: close");
-      client.println();  // Important: blank line to end header
-
-
-      //html files
-      client.println("<!DOCTYPE html>");
-      client.println("<html>");
-      client.println("<head>");
-      client.println("<title>GROUP 5 </title>");
-      client.println("</head>");
-      client.println("<body>");
-      client.println("<h1>GROUP FIVE</h1>");
-      client.println("</body>");
-      client.println("</html>");
-
-      break;
+  while (true) {
+    camera_fb_t *fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      return;
     }
-    else if(d!='\r'){
-      currentline = currentline + d;
-    }
-    else{
-      currentline ="";
-    }
-    }
+
+    response = "--frame\r\n";
+    response += "Content-Type: image/jpeg\r\n\r\n";
+    server.sendContent(response);
+    server.sendContent((const char *)fb->buf, fb->len);
+    server.sendContent("\r\n");
+
+    esp_camera_fb_return(fb);
+
+    if (!client.connected()) break;
   }
 }
 
-client.stop();
+void startCameraServer() {
+  server.on("/", HTTP_GET, []() {
+    server.send(200, "text/html",
+      "<html><body><h1>ESP32-CAM Stream</h1><img src='/stream'/></body></html>");
+  });
 
+  server.on("/stream", HTTP_GET, handleStream);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
 }
